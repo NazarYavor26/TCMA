0e3a3af3b8777fcc72c8b4c973c3cd074877ee6d
Nazar Yavorskyi <nazar.yavorskiy26@gmail.com>
2025-07-04 02:33:27 +0300

    Added validation attributes for ComponentSaveModel.

--- TCMA.BLL/Models/ComponentSaveModel.cs
+++ TCMA.BLL/Models/ComponentSaveModel.cs
@@ -1,10 +1,18 @@
-﻿namespace TCMA.BLL.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TCMA.BLL.Models
 {
     public class ComponentSaveModel
     {
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string UniqueNumber { get; set; }
+
         public bool CanAssignQuantity { get; set; }
+
+        [Range(0, int.MaxValue)]
         public int Quantity { get; set; }
     }
 }
--- TCMA.DAL/Entities/Component.cs
+++ TCMA.DAL/Entities/Component.cs
@@ -3,9 +3,13 @@
     public class Component
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+
         public string UniqueNumber { get; set; }
+
         public bool CanAssignQuantity { get; set; }
+
         public int Quantity { get; set; }
     }
 }
